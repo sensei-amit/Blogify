df3f6551ca7e0a828a1c9818f5f310c8b1b16f74
sensei-amit <geniusamit.v@gmail.com>
2025-03-11 22:41:58 +0530

    Blogify is tech blogging website developed by Amit

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCU_fIyn56lnQjZ8KREtsERfoRFF4KWEKk",
+  authDomain: "blogify-blog.firebaseapp.com",
+  projectId: "blogify-blog",
+  storageBucket: "blogify-blog.firebasestorage.app",
+  messagingSenderId: "1913848778",
+  appId: "1:1913848778:web:b1768751481141f2b58533",
+  measurementId: "G-EL697MP82H"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,35 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Blogify
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Blogify! This blog was created by Amit , 
+              kruthik , yogi and prashant project aims to share thoughts and ideas with the
+              world. we are a passionate developers who loves to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. We always learn and explore new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
